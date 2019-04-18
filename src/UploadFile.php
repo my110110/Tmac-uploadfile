a508fbfe12ba9cca15b27e867265f7d6fa3e7beb
zhengyonghui <446910086@qq.com>
2019-04-18 09:37:07 +0800

    init

---
+++ src/UploadFile.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zyh
+ * Date: 2019/4/18
+ * Time: 9:26
+ */
+namespace tmac;
+
+class UploadFile
+{
+   public function upload(int $int):string 
+   {
+       return md5($int);
+   }
+}
